40781670c8d0b5bc785da00ec6441383af0030f5
interyx <jtluensmann@gmail.com>
2024-09-30 19:48:03 -0400

    made book flexible

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def count_words(text):
     words = text.split()
     return len(words)
@@ -15,8 +18,8 @@ def count_characters(text):
     return letters
 
 
-def print_report(words, letters):
-    print("--- Beging report of books/frankenstein.txt ---")
+def print_report(filepath, words, letters):
+    print(f"--- Beginning report of {filepath} ---")
     print(f"{words} words found in the document\n")
     for letter in letters:
         print(f"The '{letter}' character was found {letters[letter]} times")
@@ -26,12 +29,22 @@ def print_report(words, letters):
 def main():
     words = 0
     letters = {}
-    with open("books/frankenstein.txt", encoding="ascii") as f:
-        file_contents = f.read()
-        words = count_words(file_contents)
-        letters = count_characters(file_contents)
-    letters = dict(sorted(letters.items(), key=lambda item: item[1], reverse=True))
-    print_report(words, letters)
+    filepath = "invalid"
+    if sys.argv[1:]:
+        filepath = sys.argv[1]
+    if filepath == "invalid":
+        print("Invalid filename")
+        sys.exit()
+    try:
+        with open(filepath, encoding="utf-8") as f:
+            file_contents = f.read()
+            words = count_words(file_contents)
+            letters = count_characters(file_contents)
+        letters = dict(sorted(letters.items(), key=lambda item: item[1], reverse=True))
+        print_report(filepath, words, letters)
+    except IOError as e:
+        print("Could not read file")
+        print(e)
 
 
 if __name__ == "__main__":
